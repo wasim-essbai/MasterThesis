00e4cce45b7bdb74bcba8bfd13eb99371ec027b8
unknown <w.essbai@studenti.unibg.it>
2023-05-22 18:53:02 +0200

    feature extraction redo

--- workspace/bp_estimation_ANN/bp_ann_train.py
+++ workspace/bp_estimation_ANN/bp_ann_train.py
@@ -14,15 +14,15 @@ warnings.filterwarnings('ignore')
 print(tf.__version__)
 print(tf.config.list_physical_devices('GPU'))
 
-data_path = 'F:/Università/Magistrale/Tesi/workspace/dataset'
-# data_path = '/content/drive/MyDrive/MasterThesis/workspace/dataset'
+# data_path = 'F:/Università/Magistrale/Tesi/workspace/dataset'
+data_path = '/content/drive/MyDrive/MasterThesis/workspace/dataset'
 
 # Loading the dataset
 dataset = pd.read_csv(f'{data_path}/dataset_part{1}.csv')
 print(f'dataset Data type: {type(dataset)}')
 print(f'dataset shape/dimensions: {dataset.shape}')
 
-X = dataset.iloc[0:, 2:].to_numpy()
+X = dataset.iloc[0:, 3:].to_numpy()
 y = dataset.iloc[0:, 0:2].to_numpy()
 
 # creating train and test sets
@@ -47,7 +47,7 @@ history = bp_ann.fit(X_train,
                      verbose=1)
 
 print("Training done!")
-bp_ann.save('./workspace/bp_estimation_ANN/model/bp_ann_model')
+bp_ann.save('./model/bp_ann_model')
 np.save('/content/drive/MyDrive/MasterThesis/workspace/ann_dataset/x_train', X_train)
 np.save('/content/drive/MyDrive/MasterThesis/workspace/ann_dataset/y_train', y_train)
 np.save('/content/drive/MyDrive/MasterThesis/workspace/ann_dataset/x_test', X_test)
--- workspace/ppg_feature_extraction/features_extraction.m
+++ workspace/ppg_feature_extraction/features_extraction.m
@@ -1,68 +1,68 @@
 clc;
 clear all;
 close all; 
-load('F:/Università/Magistrale/Tesi/workspace/dataset/part_2');
+load('F:/Università/Magistrale/Tesi/workspace/dataset/part_1');
 
-step = 1;
-h=0;
 output_file=[];
+samples_deleted = 0;
 
-if(step==1)
-    filerow_header = ["sbp" "dbp"];
-    filerow_header = [filerow_header "cp" "sut" "dt"];
-    filerow_header = [filerow_header "dt10" "st10_p_dt10" "st10_d_dt10"];
-    filerow_header = [filerow_header "dt25" "st25_p_dt25" "st25_d_dt25"];
-    filerow_header = [filerow_header "dt33" "st33_p_dt33" "st33_d_dt33"];
-    filerow_header = [filerow_header "dt50" "st50_p_dt50" "st50_d_dt50"];
-    filerow_header = [filerow_header "dt66" "st66_p_dt66" "st66_d_dt66"];
-    filerow_header = [filerow_header "dt75" "st75_p_dt75" "st75_d_dt75"];
-    output_file = [output_file;filerow_header];
-end
+filerow_header = ["sbp" "dbp"];
+filerow_header = [filerow_header "ID" "cp" "sut" "dt"];
+filerow_header = [filerow_header "dt10" "st10_p_dt10" "st10_d_dt10"];
+filerow_header = [filerow_header "dt25" "st25_p_dt25" "st25_d_dt25"];
+filerow_header = [filerow_header "dt33" "st33_p_dt33" "st33_d_dt33"];
+filerow_header = [filerow_header "dt50" "st50_p_dt50" "st50_d_dt50"];
+filerow_header = [filerow_header "dt66" "st66_p_dt66" "st66_d_dt66"];
+filerow_header = [filerow_header "dt75" "st75_p_dt75" "st75_d_dt75"];
+output_file = [output_file;filerow_header];
+
 
 Ts=1/125;
-
-sample_length = 1000;
-start_index = (step-1)*sample_length + 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tic
 f = waitbar(0,'Extracting features...');
-for d=start_index:(step*sample_length)
+for d=1:1000
     Y=p{1,d};
-    PPG=Y(1,1:1000);
+    PPG_original=Y(1,1:1000);
     BP=Y(2,1:1000);
 
-    %figure('Name','PPG and BP');
-    %subplot(2,1,1);
-    %plot(PPG);
+%     figure('Name','PPG and BP');
+%     subplot(2,1,1);
+%     plot(PPG_original);
+% 
+%     subplot(2,1,2);
+%     plot(BP);
 
-    %subplot(2,1,2);
-    %plot(BP);
-
+  
+    % Filtering    
+    windowSize = 15;
+    PPG = filter((1/windowSize)*ones(1,windowSize),1,PPG_original);
+    
     T =(0:Ts:(length(PPG)-1)*Ts); %time vector based on sampling rate
     
-    [sys_pk,sys_loc]= findpeaks(PPG,'MinPeakProminence',max(PPG)/11); % max values of PPG signal
-    [dias_pk,dias_loc]=findpeaks(-PPG,'MinPeakProminence',max(PPG)/11,'MinPeakDistance',25); % min values of PPG signal
+    [sys_pk,sys_loc]= findpeaks(PPG,'MinPeakProminence',max(PPG)/20); % max values of PPG signal
+    [dias_pk,dias_loc]=findpeaks(-PPG,'MinPeakProminence',-max(-PPG)/20); % min values of PPG signal
     dias_pk = -dias_pk;
     
     
-%    figure('Name','PPG');
-%    plot(PPG(1,1:end));
-%    hold on
-%    scatter(sys_loc, sys_pk)
-%    scatter(dias_loc, dias_pk)
-%    hold off
+%     figure('Name','PPG');
+%     plot(PPG(1,1:end));
+%     hold on
+%     scatter(sys_loc, sys_pk)
+%     scatter(dias_loc, dias_pk)
+%     hold off
     
-    [sys_bp_pk,sys_bp_loc]=findpeaks(BP); 
-    [dias_bp_pk,dias_bp_loc]=findpeaks(-BP); % min value of BP(diastole) signal    
-    dias_bp_pk = -dias_bp_pk;
-
-    %figure('Name','BP');
-    %plot(BP);
-    %hold on
-    %scatter(sys_bp_loc, sys_bp_pk)
-    %scatter(dias_bp_loc, dias_bp_pk)
-    %hold off
+%     [sys_bp_pk,sys_bp_loc]=findpeaks(BP); 
+%     [dias_bp_pk,dias_bp_loc]=findpeaks(-BP); % min value of BP(diastole) signal    
+%     dias_bp_pk = -dias_bp_pk;
+% 
+%     figure('Name','BP');
+%     plot(BP);
+%     hold on
+%     scatter(sys_bp_loc, sys_bp_pk)
+%     scatter(dias_bp_loc, dias_bp_pk)
+%     hold off
 
     %figure('Name','PPG and BP');
     %subplot(2,1,1);
@@ -78,7 +78,9 @@ for d=start_index:(step*sample_length)
         shift_index=1;
     end
     
-    last_index = min([length(sys_pk)-1, length(dias_loc)-1, length(sys_bp_loc), length(dias_bp_loc)]);
+    output_record = [];
+    %last_index = min([length(sys_pk)-1, length(dias_loc)-1, length(sys_bp_loc), length(dias_bp_loc)]);
+    last_index = min([length(sys_pk)-1, length(dias_loc)-1]);
     for k=(1+shift_index):last_index
         sys_time = T(sys_loc(1,k))-T(dias_loc(1,k-shift_index));
         dias_time = T(dias_loc(1,k+1-shift_index))-T(sys_loc(1,k));
@@ -112,17 +114,29 @@ for d=start_index:(step*sample_length)
         end
         filerow_features = [cp sys_time dias_time filerow_features];
         
-        if(any(filerow_features < 0) || any(filerow_features == 0) || any(isinf(filerow_features)))
-            continue
+        %
+        %if(any(filerow_features < 0) || any(filerow_features == 0) || any(isinf(filerow_features)))
+        %    continue
+        %end
+        bp_final_index = min(sys_loc(1,k+1)+3,1000);
+        sbp = max(BP(1,sys_loc(1,k)-3:bp_final_index));
+        abp = min(BP(1,sys_loc(1,k)-3:bp_final_index));
+        filerow_target = [sbp abp];
+        
+        if(any(filerow_features < 0))
+            output_record = [];
+            samples_deleted = samples_deleted + 1;
+            break;
         end
-        filerow_target= [sys_bp_pk(1,k) dias_bp_pk(1,k)];
-        h = h + 1;
-        output_file = [output_file;filerow_target filerow_features];
+        output_record = [output_record; d filerow_target filerow_features];
     end
-    waitbar((d-start_index)/sample_length,f,'Extracting features...');
+    
+    output_file = [output_file; output_record];
+    waitbar((d-1)/1000,f,'Extracting features...');
 end 
 
-writematrix(output_file,'dataset_part2.csv');
+
+writematrix(output_file,'dataset_part1.csv');
 %writematrix(output_file,'dataset_part1.csv','WriteMode','append');
 close(f);
 toc

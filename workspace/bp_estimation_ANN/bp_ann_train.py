d555b95ebf3114d62b02216b02c32da4b4daedc9
wasim-essbai <w.essbai@studenti.unibg.it>
2023-05-29 10:26:01 +0000

    commit from colab

--- workspace/bp_estimation_ANN/bp_ann_train.py
+++ workspace/bp_estimation_ANN/bp_ann_train.py
@@ -21,10 +21,10 @@ data_path = '/content/drive/MyDrive/MasterThesis/workspace/dataset'
 # Loading the dataset
 dataset1 = pd.read_csv(f'{data_path}/dataset_part{1}.csv')
 dataset2 = pd.read_csv(f'{data_path}/dataset_part{2}.csv')
-#dataset3 = pd.read_csv(f'{data_path}/dataset_part{3}.csv')
+dataset3 = pd.read_csv(f'{data_path}/dataset_part{3}.csv')
 #dataset4 = pd.read_csv(f'{data_path}/dataset_part{4}.csv')
 
-dataset = pd.concat([dataset1, dataset2])
+dataset = pd.concat([dataset1, dataset2, dataset3])
 print(f'dataset Data type: {type(dataset)}')
 print(f'dataset shape/dimensions: {dataset.shape}')
 
@@ -73,7 +73,7 @@ if device_name == '/device:GPU:0':
                         y_train,
                         epochs=100,
                         shuffle=True,
-                        batch_size=64,
+                        batch_size=16,
                         verbose=2)
 else:
     print('Training using CPU')

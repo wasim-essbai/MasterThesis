93b500b2d1e81aa1f693678e48e71561ee1e0382
wasim-essbai <w.essbai@studenti.unibg.it>
2023-05-31 00:49:42 +0000

    commit from colab

--- workspace/bp_estimation_ANN/bp_ann.py
+++ workspace/bp_estimation_ANN/bp_ann.py
@@ -9,11 +9,14 @@ def create_bp_ann(input_dim, activation, num_class):
 
     model.add(Dense(16, input_dim=input_dim))
     model.add(Activation(activation))
-    model.add(Dropout(0.05))
+    model.add(Dropout(0.01))
 
     model.add(Dense(8))
     model.add(Activation(activation))
-    model.add(Dropout(0.05))
+    model.add(Dropout(0.01))
+
+    model.add(Dense(4))
+    model.add(Activation(activation))
 
     model.add(Dense(num_class))
     model.add(Activation('linear'))
--- workspace/bp_estimation_ANN/bp_ann_train.py
+++ workspace/bp_estimation_ANN/bp_ann_train.py
@@ -36,14 +36,13 @@ features_to_exclude.extend(['st33_p_dt33'])
 features_to_exclude.extend(['st50_p_dt50'])
 features_to_exclude.extend(['st66_p_dt66'])
 features_to_exclude.extend(['st75_p_dt75'])
-#features_to_exclude=[]
 dataset = dataset.loc[:, ~dataset.columns.isin(features_to_exclude)]
 
 X = dataset.iloc[0:, 4:].to_numpy()
 y = dataset.iloc[0:, 1:4].to_numpy()
 
 # creating train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=True)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, shuffle=False)
 
 input_dim = X_train.shape[1]

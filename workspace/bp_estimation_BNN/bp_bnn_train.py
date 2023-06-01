24f8ea1ab4640164937fd4ae04fa91364306aac0
wasim-essbai <w.essbai@studenti.unibg.it>
2023-06-01 21:04:46 +0000

    commit from colab

--- workspace/bp_estimation_BNN/bp_bnn_train.py
+++ workspace/bp_estimation_BNN/bp_bnn_train.py
@@ -82,14 +82,14 @@ def MAE_MBP(y_true, y_pred):
     return K.mean(K.abs(mbp_true - mbp_pred))
 
 
-#bp_bnn.compile(loss='MeanAbsoluteError',
-#               optimizer=optimizers.Adam(lr=0.005),
-#               metrics=['MeanAbsolutePercentageError',
-#                        MAE_SBP, MAE_DBP, MAE_MBP])
-bp_bnn.compile(loss=negative_loglikelihood,
-               optimizer=optimizers.RMSprop(learning_rate=0.0001),
+bp_bnn.compile(loss='MeanAbsoluteError',
+               optimizer=optimizers.Adam(lr=0.004),
                metrics=['MeanAbsolutePercentageError',
                         MAE_SBP, MAE_DBP, MAE_MBP])
+#bp_bnn.compile(loss=negative_loglikelihood,
+#               optimizer=optimizers.RMSprop(learning_rate=0.001),
+#               metrics=['MeanAbsolutePercentageError',
+#                        MAE_SBP, MAE_DBP, MAE_MBP])
                         
 bp_bnn.summary()
 

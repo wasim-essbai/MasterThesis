58901f3ae4a5f126e5ba95bf5190bdf70601e33b
unknown <w.essbai@studenti.unibg.it>
2023-06-03 01:16:53 +0200

    rigen alt gwn

--- workspace/ppg_alteration/main_alteration.m
+++ workspace/ppg_alteration/main_alteration.m
@@ -5,7 +5,7 @@ prt_number = 2;
 load(strcat('F:/Università/Magistrale/Tesi/workspace/dataset/part_',int2str(prt_number)));
 
 alteration_type = 'gwn';
-wgn_sigma = 1:1:30;
+wgn_sigma = 0:0.05:1;
 
 for i=1:length(wgn_sigma)
     full_feature_extraction_alteration(Part_2, alteration_type, wgn_sigma(i), prt_number);

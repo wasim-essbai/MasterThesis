58dedf08ac115bb879b43337ef52acee66b44734
unknown <w.essbai@studenti.unibg.it>
2023-06-23 04:09:05 +0200

    evaluation for ann

--- workspace/robustness/robustness_utils.py
+++ workspace/robustness/robustness_utils.py
@@ -8,7 +8,7 @@ sys.path.append('./workspace/data/mnist_data')
 from cmnist_dataset import CMNISTDataset
 
 
-def evaluate(model, test_loader):
+def evaluate_bnn(model, test_loader):
     with torch.no_grad():
         datasetLength = len(test_loader)
         testCorrect = 0
@@ -23,7 +23,22 @@ def evaluate(model, test_loader):
         return np.round(testCorrect * 100 / len(test_loader.dataset), 2)
 
 
-def evaluate_alteration(model, alteration_name):
+def evaluate_ann(model, test_loader):
+    with torch.no_grad():
+        datasetLength = len(test_loader)
+        testCorrect = 0
+        level = 0
+
+        for data, target in test_loader:
+            pred = model(data.view(data.shape[0], -1))
+
+            pred_values = torch.max(pred, 1).indices
+            testCorrect += torch.sum(pred_values == target)
+
+        return np.round(testCorrect * 100 / len(test_loader.dataset), 2)
+
+
+def evaluate_alteration(model, alteration_name, is_bnn = True):
     base_path = f'/content/drive/MyDrive/MasterThesis/workspace/mnist_alt/{alteration_name}'
 
     dir_list = next(os.walk(base_path))[1]
@@ -37,7 +52,10 @@ def evaluate_alteration(model, alteration_name):
                           labels_root=base_path + '/',
                           transform=transforms.ToTensor()),
             batch_size=128, shuffle=False)
-        accuracy_list.append(evaluate(model, test_loader))
+        if is_bnn:
+            accuracy_list.append(evaluate_bnn(model, test_loader))
+        else:
+            accuracy_list.append(evaluate_ann(model, test_loader))
         step_list.append(float(step_dir))
         level += 1
         print('\r' + ' Evaluation: ' + str(round(100 * level / len(dir_list), 2)) + '% complete..', end="")
